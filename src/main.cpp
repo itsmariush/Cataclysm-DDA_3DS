4721f980f2dc6dc86c993164bfb5c72dd7caec3e
marius <sparer@tower.xyz>
2022-12-07 20:51:55 +0100

    Revert "Revert "adjusted font size to fit menu to screen, added keybindings""
    This reverts commit 239661f49df6882dc5f889a1791dd8659627a31e.

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 
 #ifdef __3DS__
     unsigned int __stacksize__=4*1024*1024;
+    unsigned int __ctru_heap_size = 64 * 1024 * 1024;
 #endif
 
 #include <cstring>
--- src/sdltiles.cpp
+++ src/sdltiles.cpp
@@ -2584,8 +2584,7 @@ void CheckMessages()
             }
             break;
             case SDL_JOYBUTTONDOWN:
-                printf("Joy button down %d\n", ev.jbutton.button);
-                last_input = input_event(ev.jbutton.button, CATA_INPUT_KEYBOARD);
+                last_input = input_event(ev.jbutton.button, CATA_INPUT_GAMEPAD);
             break;
             case SDL_JOYAXISMOTION: // on gamepads, the axes are the analog sticks
                 // TODO: somehow get the "digipad" values from the axes
